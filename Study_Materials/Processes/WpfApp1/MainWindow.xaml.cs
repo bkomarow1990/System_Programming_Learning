4ef559608e72f0d43552ef8be91f6210114a5e24
Gnid_ct84 <Gnid_ct84@student.itstep.org>
2021-11-16 19:44:30 +0200

    Add project files.

---
+++ Study_Materials/Processes/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // DispatcherTimer _timer = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //_timer = new DispatcherTimer();
+            //_timer.Interval = new TimeSpan(0, 0, 5);
+            //_timer.Tick += _timer_Tick;
+            //_timer.Start();
+            Load();
+        }
+
+        private void _timer_Tick(object sender, EventArgs e)
+        {
+            Load();
+        }
+
+        public void Load()
+        {
+            grid.ItemsSource = Process.GetProcesses();//.Select(p => new ProcessInfo(p));
+        }
+    }
+
+    //class ProcessInfo
+    //{
+    //    public int Id { get; set; }
+    //    public TimeSpan TotalProcessorTime { get; set; }
+    //    public ProcessPriorityClass PriorityClass { get; set; }
+    //    public string UserName { get; set; }
+
+    //    public ProcessInfo(Process pr)
+    //    {
+    //        Id = pr.Id;
+    //        try { TotalProcessorTime = pr.TotalProcessorTime; } catch { }
+    //        try { PriorityClass = pr.PriorityClass; } catch { }
+    //        UserName = GetProcessOwner(pr.Id);
+    //    }
+
+    //    public string GetProcessOwner(int processId)
+    //    {
+    //        string query = "Select * From Win32_Process Where ProcessID = " + processId;
+    //        ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
+    //        ManagementObjectCollection processList = searcher.Get();
+
+    //        foreach (ManagementObject obj in processList)
+    //        {
+    //            string[] argList = new string[] { string.Empty, string.Empty };
+    //            int returnVal = Convert.ToInt32(obj.InvokeMethod("GetOwner", argList));
+    //            if (returnVal == 0)
+    //            {
+    //                // return DOMAIN\user
+    //                return argList[0];
+    //            }
+    //        }
+
+    //        return "NO OWNER";
+    //    }
+    //}
+}
---
+++ System_Programming_Learning/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics; // Progress class
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace System_Programming_Learning
+{
+    class Program
+    {
+        static void MyMethod(object state)
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                Thread.Sleep(TimeSpan.FromMilliseconds(100));
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            #region ThreadExamples
+            //Example 1
+            //Console.WriteLine("Test1");
+            //for (int i = 0; i < 100000; i++)
+            //{
+            //    Thread.Sleep(TimeSpan.FromMilliseconds(100));
+            //}
+            //Console.WriteLine("Test2");
+
+
+            // Exmaple 2
+            //Console.WriteLine("Test1");
+            //ThreadPool.QueueUserWorkItem((el) =>
+            //{
+            //    //object
+            //    //AProcess
+            //    //Process
+            //    if (el is Process)
+            //    {
+            //        Console.WriteLine("All good it`s process");
+            //        var myProcess = el as Process;
+            //        for (int i = 0; i < 100000; i++)
+            //        {
+            //            Console.WriteLine("Test");
+            //        }
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Vasya, it`s not process");
+            //    }
+
+            //}, false);
+
+            //Console.WriteLine("Test2");
+            #endregion
+            //Processes
+
+            // Start new process with arguments 
+            //Process.Start("chrome.exe", "youtube.com");  
+
+            // Get process by name
+            Process[] processes = Process.GetProcessesByName("chrome");
+            Console.WriteLine("----------------Chrome Process----------------");
+            //Console.WriteLine(String.Join<Process>(" ", processes.ToArray())); 
+            //Console.WriteLine($" Id : {chrome.Id}\n Name: {chrome.ProcessName}\n Priority: {chrome.PriorityClass}\n Machine Name: {chrome.MachineName}\n IsExited: {chrome.HasExited}");
+            //Console.WriteLine("----------------------------------------------");
+        }
+
+
+    }
+}
+
